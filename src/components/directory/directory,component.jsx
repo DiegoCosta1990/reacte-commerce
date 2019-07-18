5b2adac26e38e9fb753578ab7206281f1c366ca3
Diego Alberto Costa <d.costa1990@outlook.com>
2019-07-18 08:54:46 -0300

    update the menu-items to navigate withRouter HOC

--- src/components/directory/directory,component.jsx
+++ src/components/directory/directory,component.jsx
@@ -10,29 +10,34 @@ class Directory extends React.Component {
             sections: [{
                 title: 'Hats',
                 imageUrl: 'https://images.pexels.com/photos/792535/pexels-photo-792535.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'Jackets',
                 imageUrl: 'https://images.unsplash.com/photo-1545594861-3bef43ff2fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'Sneakers',
                 imageUrl: 'https://images.unsplash.com/photo-1495555961986-6d4c1ecb7be3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {
                 title: 'Womans',
-                imageUrl: 'https://images.unsplash.com/photo-1489939078242-0a1dc4a08f06?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                imageUrl: 'https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {
                 title: 'Mens',
                 imageUrl: 'https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }
         ]
         }
@@ -42,8 +47,8 @@ class Directory extends React.Component {
         return(
             <div className= 'directory-menu'>
                 { 
-                    this.state.sections.map(({title, imageUrl, id, size}) => ( 
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)) 
+                    this.state.sections.map(({id, ...otherSectionsProps}) => ( 
+                    <MenuItem key={id} {...otherSectionsProps}/>)) 
                 }
             </div>
         )
